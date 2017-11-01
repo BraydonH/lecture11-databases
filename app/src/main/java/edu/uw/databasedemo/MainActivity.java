cdab62f464337ae1de4a1a29fc184f18f9ef142b
Joel Ross <joelross@uw.edu>
2017-11-01 09:14:01 -0700

    Add completed demo

--- app/src/main/java/edu/uw/databasedemo/MainActivity.java
+++ app/src/main/java/edu/uw/databasedemo/MainActivity.java
@@ -1,12 +1,12 @@
 package edu.uw.databasedemo;
 
-import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.UserDictionary;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 this,
                 R.layout.list_item_layout, //item to inflate
                 null, //cursor to show
-                new String[] {UserDictionary.Words.WORD, UserDictionary.Words.FREQUENCY}, //fields to display
+                new String[] {WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT}, //fields to display
                 new int[] {R.id.txt_item_word, R.id.txt_item_freq},                       //where to display them
                 0); //flags
         listView.setAdapter(adapter);
@@ -70,8 +70,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Cursor item = (Cursor)parent.getItemAtPosition(position); //item we clicked on
-                String word = item.getString(item.getColumnIndexOrThrow(UserDictionary.Words.WORD));
-                int freq = item.getInt(item.getColumnIndexOrThrow(UserDictionary.Words.FREQUENCY));
+                String word = item.getString(item.getColumnIndexOrThrow( WordDatabase.WordEntry.COL_WORD ));
+                int freq = item.getInt(item.getColumnIndexOrThrow( WordDatabase.WordEntry.COL_COUNT ));
                 Log.v(TAG, "Clicked on '"+word+"' ("+freq+")");
 
                 setFrequency(id, freq+1);
@@ -83,12 +83,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
-        String[] projection = new String[] { UserDictionary.Words.WORD, UserDictionary.Words.FREQUENCY, UserDictionary.Words._ID };
+        String[] projection = new String[] { WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT, WordDatabase.WordEntry._ID };
 
         //create the CursorLoader
         CursorLoader loader = new CursorLoader(
                 this,
-                UserDictionary.Words.CONTENT_URI,
+                WordProvider.CONTENT_URI,
                 projection,
                 null,
                 null,
@@ -113,13 +113,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     //adds (creates) a word to the list
     public void addWord(String word){
         ContentValues newValues = new ContentValues();
-        newValues.put(UserDictionary.Words.WORD, word);
-        newValues.put(UserDictionary.Words.FREQUENCY, 100);
-        newValues.put(UserDictionary.Words.APP_ID, "edu.uw.loaderdemo");
-        newValues.put(UserDictionary.Words.LOCALE, "en_US");
+        newValues.put(WordDatabase.WordEntry.COL_WORD, word);
+        newValues.put(WordDatabase.WordEntry.COL_COUNT, 100);
 
         Uri newUri = this.getContentResolver().insert(
-                UserDictionary.Words.CONTENT_URI,   // the user dictionary content URI!
+                WordProvider.CONTENT_URI,   // the user dictionary content URI!
                 newValues                   // the values to insert
         );
         Log.v(TAG, "New word at: "+newUri);
@@ -128,10 +126,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     //sets (updates) the frequency of the word with the given id
     public void setFrequency(long id, int newFrequency){
         ContentValues newValues = new ContentValues();
-        newValues.put(UserDictionary.Words.FREQUENCY, newFrequency);
+        newValues.put( WordDatabase.WordEntry.COL_COUNT, newFrequency);
 
         this.getContentResolver().update(
-                ContentUris.withAppendedId(UserDictionary.Words.CONTENT_URI, id),
+                ContentUris.withAppendedId( WordProvider.CONTENT_URI, id),
                 newValues,
                 null, null); //no selection
     }
@@ -149,17 +147,20 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         switch(item.getItemId()){
             case R.id.menu_test:
 
-                //demo: query the provider
-                ContentResolver resolver = getContentResolver();
-                String[] projection = new String[] {
-                        UserDictionary.Words.WORD,
-                        UserDictionary.Words.FREQUENCY,
-                        UserDictionary.Words._ID
-                };
-                Cursor results = resolver.query(UserDictionary.Words.CONTENT_URI, projection, null, null, null);
+                WordDatabase.DatabaseHelper helper = new WordDatabase.DatabaseHelper(this);
+                SQLiteDatabase db = helper.getReadableDatabase();
+
+                SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+                builder.setTables(WordDatabase.WordEntry.TABLE_NAME);
+
+                Cursor results = builder.query(
+                        db,
+                        new String[] {WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT},
+                        null, null, null, null, null);
+
                 while(results.moveToNext()){
-                    String word = results.getString(results.getColumnIndexOrThrow( UserDictionary.Words.WORD )); //get the "word" field as a String
-                    int freq = results.getInt(results.getColumnIndexOrThrow( UserDictionary.Words.FREQUENCY ));
+                    String word = results.getString(results.getColumnIndexOrThrow( WordDatabase.WordEntry.COL_WORD )); //get the "word" field as a String
+                    int freq = results.getInt(results.getColumnIndexOrThrow( WordDatabase.WordEntry.COL_COUNT ));
                     Log.v(TAG, "'"+word+"' ("+freq+")");
                 }
 
--- app/src/main/java/edu/uw/databasedemo/WordDatabase.java
+++ app/src/main/java/edu/uw/databasedemo/WordDatabase.java
@@ -1,6 +1,11 @@
 package edu.uw.databasedemo;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+import android.util.Log;
 
 /**
  * A class for managing a database of words
@@ -28,15 +33,50 @@ public class WordDatabase {
         public static final String COL_COUNT = "count";
     }
 
-    //SQL for creating a table
-    private static final String CREATE_TASKS_TABLE =
-            "CREATE TABLE " + WordEntry.TABLE_NAME + "(" +
-                    WordEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "+
-                    WordEntry.COL_WORD + " TEXT" + ","+
-                    WordEntry.COL_COUNT + " INTEGER" +
-                    ")";
+    /**
+     * A class to help open, create, and update the database
+     */
+    public static class DatabaseHelper extends SQLiteOpenHelper {
 
-    //SQL for dropping a table
-    private static final String DROP_TASKS_TABLE = "DROP TABLE IF EXISTS "+ WordEntry.TABLE_NAME;
+        //SQL for creating a table
+        private static final String CREATE_TASKS_TABLE =
+                "CREATE TABLE " + WordEntry.TABLE_NAME + "(" +
+                        WordEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "+
+                        WordEntry.COL_WORD + " TEXT" + ","+
+                        WordEntry.COL_COUNT + " INTEGER" +
+                        ")";
+
+        //SQL for dropping a table
+        private static final String DROP_TASKS_TABLE = "DROP TABLE IF EXISTS "+ WordEntry.TABLE_NAME;
+
+
+        public DatabaseHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.v(TAG, "Creating tasks table");
+            db.execSQL(CREATE_TASKS_TABLE); //create table if needed
+
+            //add sample words
+            ContentValues sample1 = new ContentValues();
+            sample1.put(WordEntry.COL_WORD, "Embiggen");
+            sample1.put(WordEntry.COL_COUNT, 0);
+            ContentValues sample2 = new ContentValues();
+            sample2.put(WordEntry.COL_WORD, "Cromulent");
+            sample2.put(WordEntry.COL_COUNT, 0);
+
+            db.insert(WordEntry.TABLE_NAME, null, sample1);
+            db.insert(WordEntry.TABLE_NAME, null, sample2);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(DROP_TASKS_TABLE); //just drop and recreate table
+            onCreate(db);
+        }
+    }
+
 
 }
